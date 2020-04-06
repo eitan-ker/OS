3cf51c1bfc93afbab54b7b1c4ea0692c6203cb05
eitan-ker <eizzker@gmail.com>
2020-04-06 23:27:59 +0300

    finished ex13. checked on submit: ex11 - ok, ex12 - add line numbers, ex13 - check what's wrong

--- ex1/ex12/ex12.sh
+++ ex1/ex12/ex12.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+line=0
 ls $1 | grep .txt | sort | sed 's/.*/& is a file/'
 ls $1 | grep -F -v "." | sort | sed 's/.*/& is a directory/'
---
+++ ex1/ex13/ex13.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+ls $1 | grep -r $2 * | sort
