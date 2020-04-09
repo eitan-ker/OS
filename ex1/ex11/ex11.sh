39d84c007a06b15d6480f223834f3224558a2749
eitan-ker <eizzker@gmail.com>
2020-04-09 12:40:19 +0300

    ex11.sh done++

--- ex1/ex11/ex11.sh
+++ ex1/ex11/ex11.sh
@@ -3,12 +3,16 @@
 # 205697139
 
 input=$1
+# line counter
 numLine=0
+# line reader
 while IFS= read -r line
 do
+# num of words
     words=0
     counter=0
     numLine=$[$numLine+1]
+# words reader
     for word in $line; do
         words=$[$words+1]
         if [ $word = $2 ]
