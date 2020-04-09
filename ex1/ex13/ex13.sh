cc8c25621058733230014dfc3b8826c1f7be77d6
eitan-ker <eizzker@gmail.com>
2020-04-09 12:34:39 +0300

    ex13.sh done++

--- ex1/ex13/ex13.sh
+++ ex1/ex13/ex13.sh
@@ -2,18 +2,21 @@
 # Eitan Kerzhner
 # 205697139
 
+# running on all files in the first level folder
 for d in $(ls $1 | sort);
 do
+# if there is a file in the first folder
     if [[ $d = $2 ]]
         then
             cat $1/$d;
             echo
     else
-        # recursive call
         if [[ $d != *"."* ]]
             then
+# running on all files in the second level folder
                 for n in $(ls $1/$d | sort);
                 do
+# if ther's a file in the second level folders
                     if [[ $n = $2 ]]
                     then
                         cat $1/$d/$n;
