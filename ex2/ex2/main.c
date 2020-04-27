57975cc81312e126692509b3b00a181ea69fa4bc
eitan-ker <eizzker@gmail.com>
2020-04-27 19:04:00 +0300

    parsing command

--- ex2/ex2/main.c
+++ ex2/ex2/main.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/types.h>
+
 
 int main() {
-    printf("Hello, World!\n");
+    char prompt[3] = "> ";
+    char input[100], dummy;
+    char* command, temp;
+    int i =0;
+    // running the shell
+    while (true) {
+        printf("%s", prompt);
+        scanf("%[^\n]s",input);
+        scanf("%c" ,&dummy);
+        command = strtok(input, " ");
+        temp = strtok(NULL, " ");
+
+    }
     return 0;
 }
