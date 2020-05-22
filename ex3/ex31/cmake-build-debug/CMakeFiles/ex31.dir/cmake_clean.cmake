a061642c908edecf91d4572b6835949d35d242f5
eitan-ker <eizzker@gmail.com>
2020-05-22 13:17:37 +0300

    started working on ex3

---
+++ ex3/ex31/cmake-build-debug/CMakeFiles/ex31.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ex31.dir/main.c.o"
+  "ex31"
+  "ex31.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ex31.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
