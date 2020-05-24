1501f72f334f17b6ff067d9a0b7e7af111197769
eitan-ker <eizzker@gmail.com>
2020-05-24 10:48:22 +0300

    added last changes after reading notes - now completed

--- ex3/ex31/main.c
+++ ex3/ex31/main.c
@@ -4,7 +4,7 @@
 #include <sys/stat.h>
 #include <unistd.h>
 
-int checkFIles(char *file1, char *file2, int file1_size, int file2_size);
+int checkFiles(char *file1, char *file2, int file1_size, int file2_size);
 
 int checkCorrespondence(char buff1[1024], char buff2[1024], int size1, int size2);
 
@@ -51,7 +51,7 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    ret_value = checkFIles(file1, file2, file1_size, file2_size);
+    ret_value = checkFiles(file1, file2, file1_size, file2_size);
 
     close(file1);
     close(file2);
@@ -62,42 +62,17 @@ int main(int argc, char *argv[]) {
 // returns the number of matches
 int checkCorrespondence(char buff1[1024], char buff2[1024], int size1, int size2) {
     int i, counter = 0, minSize = 0;
-    int maxCount = 0;
-    if (size1 == size2) { // might be identical
-        // identical check
-        for (i = 0; i < size1; i++) {
-            char a = buff1[i];
-            char b = buff2[i];
-            if (buff1[i] == buff2[i]) {
-                counter++;
-            } else {
-                if (maxCount < counter) {
-                    maxCount = counter;
-                    counter = 0;
-                }
-            }
-        }
-    } else { // might be similar
-        minSize = min(size1, size2);
-        for (i = 0; i < minSize; i++) {
-            if (buff1[i] == buff2[i]) {
-                counter++;
-            } else {
-                if (maxCount < counter) {
-                    maxCount = counter;
-                    counter = 0;
-                }
-            }
+    minSize = min(size1, size2); // return min is different size, or one of them if the same size
+    for (i = 0; i < minSize; i++) {
+        if (buff1[i] == buff2[i]) {
+            counter++;
         }
     }
-    if (maxCount == 0) {
-        return counter;
-    }
-    return maxCount;
+    return counter;
 }
 
 // might be identical / similar / different
-int checkFIles(char *file1, char *file2, int file1_size, int file2_size) {
+int checkFiles(char *file1, char *file2, int file1_size, int file2_size) {
     int flag; // locally: 1 - identical, 3 - similar, 2 - different
     int matches = 0, maxMatches = 0, matchesCounter = 0, maxBufSize = 0;
     int i = 0, j = 0, minFileSize, maxFileSize;
@@ -112,8 +87,12 @@ int checkFIles(char *file1, char *file2, int file1_size, int file2_size) {
 
         for (i = 0; i < file1_size; i++) {
             for (j = 0; j < file2_size; j++) {
-                lseek(file1, i, SEEK_SET);
-                lseek(file2, j, SEEK_SET);
+                if(lseek(file1, i, SEEK_SET == -1)) {
+                    fprintf(stderr, "there is an error in lseek function in file 1");
+                }
+                if (lseek(file2, j, SEEK_SET == -1)) {
+                    fprintf(stderr, "there is an error in lseek function in file 2");
+                }
                 buff1_size = read(file1, buff1, 1024);
                 buff2_size = read(file2, buff2, 1024);
                 if (i == 0) {
